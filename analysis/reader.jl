c5f05cedae91c1fd65c3391363816bf5993a4417
Aaron Nielsen <apn@apnielsen.com>
2021-06-13 13:20:16 -0400

    updatd reader plot

--- analysis/reader.jl
+++ analysis/reader.jl
@@ -12,6 +12,10 @@ using PyPlot
 # https://github.com/JuliaLang/julia/issues/31525
 # https://discourse.julialang.org/t/dates-datetime-parse-string-with-microseconds-decimal-6-places/25653
 
+# the default pi timezone is BST (British summer time)
+# changed to US/Eastern in Early map
+filename = "2021-04-09-log.csv"
+
 dateformat= DateFormat("Y-m-d H:M:S.sss")
 
 function DateTimeMs(usstr)
@@ -22,7 +26,7 @@ function celcius2farenheit(c)
     c*9/5+32
 end
          
-df = CSV.read("log.csv",
+df = CSV.read(filename,
               header=["datetime","eC02","TVOC","PM25","SCD30_CO2","SCD30_Temp","SCD30_RH","BMP280_Temp","BMP280_Pres","BMP280_Alt"],
               DataFrame)
 
@@ -59,4 +63,21 @@ grid()
 gcf().autofmt_xdate()
 title("Relative Humidity")
 ylabel("Relative Humidity")
+xlabel("Time")
+
+
+figure()
+plot(df.dt,df.TVOC)
+grid()
+gcf().autofmt_xdate()
+title("VOCs")
+ylabel("Total VOC")
+xlabel("Time")
+
+figure()
+plot(df.dt,df.eC02)
+grid()
+gcf().autofmt_xdate()
+title("eCO2")
+ylabel("eCO2")
 xlabel("Time")
